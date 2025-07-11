1b62a1013405c1871b04a314349ec57226b951a9
Sahil Kumar Jha <striversahil@gmail.com>
2025-07-11 11:24:07 +0530

    built streamable chat interface

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { GoogleGenAI } from "@google/genai"
 import { config } from "dotenv"
+import readline from "readline-sync"
 // Load environment variables from .env file
 config()
 
@@ -10,25 +11,69 @@ const genai = new GoogleGenAI({
 
 const systemMessage = `
 You are a helpful assistant that can answer questions about the Google Gemini API and how to use it in JavaScript applications. Provide clear and concise explanations, examples, and best practices for using the API effectively.
+
 `
+const History : { role : string , parts : { text : string }[] }[] = []
 
 
+const runAgent = async (message: string) => {
+    let responseText = ""
 
-
-const chat = genai.chats.create({
-    model: "gemini-2.5-flash",
-    config: {
-        systemInstruction: systemMessage,
-    }
-})
-
-
-async function main() {
     try {
-        const response = await chat.sendMessage({ message : "How are Your ?" })
-        console.log("Response from Google GenAI:\n", response.text)
+        const response = await genai.models.generateContentStream({
+            model: "gemini-2.0-flash",
+            contents: History,
+            config: {
+                systemInstruction: systemMessage,
+                maxOutputTokens: 1000,
+            }
+        })
+        
+        for await (const chunk of response) {
+            if (chunk.text) {
+                responseText += chunk.text
+                console.log(chunk.text)
+            }
+        }
+        return responseText
+
     } catch (error) {
         console.error("Error sending message to Google GenAI:", error)
     }
 }
+
+
+
+
+async function main() {
+    const message = readline.question("Enter your Query to Google GenAI: \n")
+    if (message.toLocaleLowerCase() === "exit" || message.toLocaleLowerCase() === "quit") {
+        console.log("Exiting the conversation. Goodbye! 👋 \n")
+        // sleep for 1 second before exiting
+        await new Promise(resolve => setTimeout(resolve, 1000))
+        return
+    }
+
+    try {
+        History.push({
+            role: "user",
+            parts: [{ text: message }]
+        })
+
+        const response = await runAgent(message)
+        if (response) {
+            History.push({
+                role: "assistant",
+                parts: [{ text: response }]
+            })
+        } else {
+            console.log("No response received from Google GenAI.")
+        }
+
+        main() // Call main again to continue the conversation
+    } catch (error) {
+        console.error("Error sending message to Google GenAI:", error)
+    }
+}
+
 main()
